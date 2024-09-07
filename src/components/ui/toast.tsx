6b1ef22be4b1325ce935664efa8f16aeebb2763c
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2024-09-07 10:12:15 +0000

    fixed toast className for break word

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -106,7 +106,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
-    className={cn("text-sm opacity-90", className)}
+    className={cn("text-sm opacity-90 break-normal break-words text-ellipsis break-all", className)}
     {...props}
   />
 ))
